05bdc446e9af927056db0d3ecdc889f7a94bd5f5
miyujach <miyu.4630@gmail.com>
2017-05-24 10:56:55 +0000

    Implementation du code avec le robot

--- thread2.py
+++ thread2.py
@@ -10,15 +10,18 @@ import threading
 import sys
 
 moveSleepDelay = 0.1
-moveSteps = 10
+moveSteps = 20
 
-pinRoueGauche = 7
-pinRoueDroite = 5
-pinRoueAvancer = 3
-pinAttraperRelacher = 11
-pinCamGauche = 15
-pinCamDroite = 13
+pinRoueDroite = 5			#A0
+pinRoueGauche = 7 			#A1
+pinRoueAvancer = 3			#A2
+pinAttraperRelacher = 11	#A3
+pinCamGauche = 15			#A4
+pinCamDroite = 13			#A5
+
 headMoveDirection = True # True = Gauche, False = Droite
+canetteTrouvee = False
+nombreDeRotationCameraPourLaRecherche = 0
 
 mutexActionRotationHead = threading.Event()
 mutexHead = threading.Event()
@@ -35,6 +38,7 @@ face_cascade = cv2.CascadeClassifier('/home/pi/opencv_regular/opencv-3.2.0/data/
 camera = PiCamera()
 camera.resolution = (resolutionX, resolutionY)
 camera.framerate = 32
+camera.rotation = 90
 rawCapture = PiRGBArray(camera, size=camera.resolution )
 # allow the camera to warmup
 time.sleep(0.1)
@@ -56,98 +60,121 @@ def setup():
 
 
 oldPin = 200
-def actionRoues(pin):
+def actionRoues(pin = ""):
 	if pin == 200 and oldPin != 200:
 		#Eteind la led	
 		GPIO.setup(oldPin,GPIO.OUT)
-		print "LED off"
+		#print "LED off"
 		GPIO.output(oldPin,GPIO.LOW)
 	
 	
 	if oldPin == pin and oldPin != 200:
-		print "Allume la led"
 		GPIO.setup(pin,GPIO.OUT)
-		print "startActionRoues"
+		#print "startActionRoues"
 		GPIO.output(pin,GPIO.HIGH)
 	elif oldPin != 200:
 		#Eteind la led
 		GPIO.setup(oldPin,GPIO.OUT)
-		print "endActionRoues"
+		#print "endActionRoues"
 		GPIO.output(oldPin,GPIO.LOW)
 	
+	
+	# Arrete toutes les roues
+	if pin == "":
+		GPIO.output(pinRoueDroite,GPIO.LOW)
+		GPIO.output(pinRoueGauche,GPIO.LOW)
+		GPIO.output(pinRoueAvancer,GPIO.LOW)
+		
 	global oldPin 
 	oldPin= pin
 	return
 
 
 
-def actionRotationCamera(actionCam, temps = 0):
-	print temps
+def actionRotationCamera(actionCam, temps = 0.5):
+	#print temps
 	wait = int(temps)
 	
 	if wait != 0:
 		threadFinished = False
 	
+	global pinCamGauche
+	global pinCamDroite
 	
 	if actionCam == pinCamGauche:
-		GPIO.output(pinCamDroite,GPIO.HIGH)
-		
-		time.sleep(wait)
-		#print "Cam Gauche"
-		GPIO.output(pinCamGauche,GPIO.LOW)
+		GPIO.output(pinCamGauche,GPIO.HIGH)
+		print "Tourne la camera sur la [[ GAUCHE ]] afin de mettre la canette au centre"
 		
 	elif actionCam == pinCamDroite:
 		GPIO.output(pinCamDroite,GPIO.HIGH)
-		
-		time.sleep(wait)
-		#print "Cam Droite"
-		GPIO.output(pinCamGauche,GPIO.LOW)
+		print "Tourne la camera sur la [[ DROITE ]] afin de mettre la canette au centre"
+
 	
+	time.sleep(wait)
+	
+	GPIO.output(pinCamDroite,GPIO.LOW)
+	GPIO.output(pinCamGauche,GPIO.LOW)
 		
 
 def pinAttraperRelacherCanette(str):
 	GPIO.setup(pinAttraperRelacher,GPIO.OUT)
 	if str == "attraper":
-		print "Attraper la canette"
+		print "[[ Attraper canette ]] Action Robot"
 		GPIO.output(pinAttraperRelacher,GPIO.HIGH)
 	elif str == "relacher":
-		print "Relacher la canette"
+		print "[[ Relacher canette ]] Action Robot"
 		GPIO.output(pinAttraperRelacher,GPIO.LOW)
 
 
 
-
-
 def threadMoveHead(mutexHead):
-	global headMoveDirection, moveSleepDelay
-	counter = moveSteps
-	while True:
-		# Thread is disabled
-		if mutexHead.wait(moveSleepDelay):
-			global halt
-			if halt:
-				print "[HALT] Head thread"
-				break
-			time.sleep(moveSleepDelay * 2)
-			continue
-		# Inversion de la direction
-		if counter == moveSteps or counter == -moveSteps:
-			headMoveDirection = not headMoveDirection
-			print "Nouvelle direction:", ("Gauche" if headMoveDirection else "Droite")
-			if headMoveDirection:
-				print "Set pins GAUCHE = 1"
-				GPIO.output(pinCamGauche, GPIO.HIGH)
-				GPIO.output(pinCamDroite, GPIO.LOW)
-			else:
-				print "Set pins DROITE = 1"
-				GPIO.output(pinCamGauche, GPIO.LOW)
-				GPIO.output(pinCamDroite, GPIO.HIGH)
-		# Increment
-		counter += (1 if headMoveDirection else -1)
-		#print "Counter:", counter
+	global canetteTrouvee
+	
+	if not canetteTrouvee:
+		global headMoveDirection, moveSleepDelay
+		counter = moveSteps
+		while True:
+			# Thread is disabled
+			if mutexHead.wait(moveSleepDelay):
+				global halt
+				if halt:
+					print "[HALT] Head thread"
+					break
+				time.sleep(moveSleepDelay * 2)
+				continue
+			# Inversion de la direction
+			if counter == moveSteps or counter == -moveSteps:
+				nombreDeRotationCameraPourLaRecherche = nombreDeRotationCameraPourLaRecherche + 1
+				headMoveDirection = not headMoveDirection
+				print "Nouvelle direction de la camera pour recherche:", ("GAUCHE" if headMoveDirection else "DROITE")
+				
+				global nombreDeRotationCameraPourLaRecherche 
+				print "nombreDeRotationCameraPourLaRecherche", nombreDeRotationCameraPourLaRecherche
+				
+				if nombreDeRotationCameraPourLaRecherche > 3:
+					print "Le robot tourne sur la droite car il n'a pas trouvee de canette pendant sa recherche"
+					nombreDeRotationCameraPourLaRecherche = 0
+					actionRoues(pinRoueGauche)
+					
+					
+					time.sleep(moveSleepDelay * 20)
+					continue
+				
+				if headMoveDirection:
+					#print "Set pins GAUCHE = 1"
+					GPIO.output(pinCamGauche, GPIO.HIGH)
+					GPIO.output(pinCamDroite, GPIO.LOW)
+				else:
+					#print "Set pins DROITE = 1"
+					GPIO.output(pinCamGauche, GPIO.LOW)
+					GPIO.output(pinCamDroite, GPIO.HIGH)
+			# Increment
+			counter += (1 if headMoveDirection else -1)
+			#print "Counter:", counter
+
 
 def threadScanVideo(mutexHead, mutexVideo):
-	print "VIDEO"
+	#print "VIDEO"
 	
 	
 	'''
@@ -210,23 +237,23 @@ def threadScanVideo(mutexHead, mutexVideo):
 		# finding centroids of best_cnt and draw a circle there
 		M = cv2.moments(best_cnt)
 		cx,cy = int(M['m10']/M['m00']), int(M['m01']/M['m00'])
-		#if best_cnt>1:
-		cv2.circle
-
-		(blur,(cx,cy),10,(0,0,255),-1)
-
+		cv2.circle(blur,(cx,cy),10,(0,0,255),-1)	
+		
+		#Si centreCercle = [0, 0] -> Aucune canette trouvee!
 		centreCercle = [cx,cy]
-		
-		
 		
 		
 		if centreCercle == [0,0]:
 			mutexHead.clear() # Relance le threadMoveHead
-			print "no target"
+			print "Aucune canette trouvee"
+			global canetteTrouvee
+			canetteTrouvee = False
 			
 		else:
 			mutexHead.set() # Stop le threadMoveHead 
-			print "Ok"
+			print "Canette trouvee"
+			global canetteTrouvee
+			canetteTrouvee = True
 
 
 
@@ -236,36 +263,41 @@ def threadScanVideo(mutexHead, mutexVideo):
 		##########
 		# 2 - Get la position de la canette sur l ecran
 		##########
-		'''
-				
+		'''	
 		#Centre de l'ecran
 		centerX = resolutionX / 2
 		centerY = resolutionY / 2
 		
-		if(centreCercle[0] < centerX-40):
+		#Si le robot trouve la canette
+		# 1 - Cherche a aligner le robot face a la canette
+		#	-	Camera suit la canette, elle cherche a centre la canette au centre de la camera
+		#	-	Le robot s'oriente (tourne)
+		# 2 - Le robot avance
+		
+		if(centreCercle[0] < centerX-40 and centreCercle[0] != 0):
 			#Canette a gauche
 			cv2.putText(image, "Gauche", (150, 230), font, 0.5, (255,255,255),2,cv2.LINE_AA)
 			actionRoues(pinRoueGauche)
 			actionRotationCamera(pinCamGauche)
-			print "Cam Gauche"
+			print "[[ Gauche ]] - Direction Robot"
 
-		elif(centreCercle[0] > centerX+40):
+		elif(centreCercle[0] > centerX+40 and centreCercle[0] != 0):
 			#Canette a droite
 			cv2.putText(image, "Droite", (150, 230), font, 0.5, (255,255,255),2,cv2.LINE_AA)
 			actionRoues(pinRoueDroite)			
 			actionRotationCamera(pinCamDroite)
-			print "Cam Droite"
+			print "[[ Droite ]] - Direction Robot"
 
-		elif(centreCercle[0] > centerX-40 and centreCercle[0] < centerX+40):
+		elif(centreCercle[0] > centerX-40 and centreCercle[0] < centerX+40 and centreCercle[0] != 0):
 			#Canette au centre
 			cv2.putText(image, "Avancer", (150, 230), font, 0.5, (255,255,255),2,cv2.LINE_AA)
 			actionRoues(pinRoueAvancer)
-			print "Avancer"
-
-
+			print "[[ Avancer ]] - Direction Robot"
 
 		'''-----------------------'''
 
+
+		
 
 		'''
 		##########
@@ -276,7 +308,7 @@ def threadScanVideo(mutexHead, mutexVideo):
 			cnt2 = contours[np.argmax(areas)]
 			tx,ty,tw,th = cv2.boundingRect(cnt2)
 			cv2.rectangle(blur,(tx,ty),(tx+tw,ty+th),(0,255,255),2)
-			print "ok"
+
 
 			if tw*th > 26000:
 				pinAttraperRelacherCanette("attraper")
@@ -305,7 +337,7 @@ def threadScanVideo(mutexHead, mutexVideo):
 		if mutexVideo.wait(1):
 			print "[HALT] Video thread"
 			break
-		print "Video"
+		#print "Video"
 	
 # Interrupt Signal
 halt = False
